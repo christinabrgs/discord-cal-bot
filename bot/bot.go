acd459e7f1a34c2853a877bf8611297541fd283b
Christina Burgos <christina.brgs@gmail.com>
2025-11-28 01:44:03 -0500

    subscribe is now parsing the ical

--- bot/bot.go
+++ bot/bot.go
@@ -3,14 +3,61 @@ package bot
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
+	"time"
 
+	ics "github.com/arran4/golang-ical"
 	"github.com/bwmarrin/discordgo"
 )
 
-var BotToken string
+var (
+	BotToken string
+	s        *discordgo.Session
+)
+
+type Commands interface {
+	Subscribe(url string) ([]Event, error)
+	Unsubscribe(url string)
+	Filter(word string)
+}
+
+type Event struct {
+	Name        string
+	Description string
+	StartTime   time.Time
+	EndTime     time.Time
+}
+
+type Cal struct {
+	Events []Event
+}
+
+func (c Cal) Subscribe(url string) ([]Event, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	cal, err := ics.ParseCalendar(resp.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return nil, err
+	}
+	fmt.Println(cal.Events())
+	return nil, nil
+}
+
+func (c Cal) Unsubscribe(url string) {
+	fmt.Print("not implemented")
+}
+
+func (c Cal) Filter(url string) {
+	fmt.Print("not implemented")
+}
 
 func checkNilErr(e error) {
 	if e != nil {
@@ -21,6 +68,11 @@ func checkNilErr(e error) {
 func Run() {
 	discord, err := discordgo.New("Bot " + BotToken)
 	checkNilErr(err)
+
+	d := Cal{}
+	d.Subscribe("https://api2.luma.com/ics/get?entity=calendar&id=cal-s0hIkjIqVvTXRBG")
+	// d.Unsubscribe("string")
+	// d.Filter("string")
 
 	discord.AddHandler(newMessage)
 
--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module git.phlcode.club/discord-bot
 go 1.25.0
 
 require (
-	github.com/bwmarrin/discordgo v0.29.0 // indirect
+	github.com/arran4/golang-ical v0.3.2
+	github.com/bwmarrin/discordgo v0.29.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
