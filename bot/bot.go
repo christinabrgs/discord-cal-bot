5f05c940c84e0a2260cfcabd69b1f00a81b23f8e
Christina Burgos <christina.brgs@gmail.com>
2025-12-09 21:00:55 -0500

    so close yet so far away

--- bot/bot.go
+++ bot/bot.go
@@ -12,11 +12,11 @@ import (
 	"os"
 	"os/signal"
 	"regexp"
-	"strings"
 	"time"
 
 	"git.phlcode.club/discord-bot/database"
 	f "git.phlcode.club/discord-bot/dbFunctions"
+	"git.phlcode.club/discord-bot/types"
 	ics "github.com/arran4/golang-ical"
 	"github.com/bwmarrin/discordgo"
 )
@@ -34,15 +34,6 @@ const (
 	LocationField    filterField = "location"
 )
 
-func handleICSProp(prop *ics.IANAProperty, required bool, handler func(val string) error) error {
-	if prop != nil {
-		return handler(prop.Value)
-	} else if required {
-		return errMissingProperty
-	}
-	return nil
-}
-
 type Commands interface {
 	Subscribe(url string, guildID string) error
 	Unsubscribe(url string) error
@@ -56,73 +47,6 @@ type Event struct {
 	StartTime   time.Time
 	EndTime     time.Time
 	Location    string
-}
-
-func ParseTime(value string) (time.Time, error) {
-	if strings.HasSuffix(value, "Z") {
-		time, err := time.Parse(`20060102T150405Z`, value)
-		if err != nil {
-			return time, fmt.Errorf("unable to parse time: %s", err.Error())
-		}
-		return time, nil
-	}
-	time, err := time.Parse(`20060102T150405`, value)
-	if err != nil {
-		return time, fmt.Errorf("unable to parse time: %s", err.Error())
-	}
-	return time, nil
-}
-
-func (e *Event) ParseFromiCal(event *ics.VEvent) error {
-	err := handleICSProp(event.GetProperty(ics.ComponentPropertySummary), true, func(val string) error {
-		e.Name = val
-		return nil
-	})
-	if err != nil {
-		return errors.New("name (summary) is required but missing from event")
-	}
-	err = handleICSProp(event.GetProperty(ics.ComponentPropertyDtStart), true, func(val string) error {
-		startTime, err := ParseTime(val)
-		if err != nil {
-			return fmt.Errorf("unable to parse start time: %s", err.Error())
-		}
-		e.StartTime = startTime
-		return nil
-	})
-	if err != nil {
-		if errors.Is(err, errMissingProperty) {
-			return errors.New("start time is required but missing from event")
-		}
-		return errors.Join(errors.New("error handling start time: "), err)
-	}
-	err = handleICSProp(event.GetProperty(ics.ComponentPropertyDtEnd), false, func(val string) error {
-		endTime, err := ParseTime(val)
-		if err != nil {
-			return fmt.Errorf("unable to parse end time: %s", err.Error())
-		}
-		e.EndTime = endTime
-		return nil
-	})
-	if err != nil {
-		return errors.Join(errors.New("error handling end time: "), err)
-	}
-	err = handleICSProp(event.GetProperty(ics.ComponentPropertyDescription), false, func(val string) error {
-		e.Description = val
-		return nil
-	})
-	if err != nil {
-		slog.Default().Warn("Err was not nil when parsing optional event description", "error", err)
-		// This is purposefull empty because we should never get here since this isn't required
-	}
-	err = handleICSProp(event.GetProperty(ics.ComponentPropertyLocation), false, func(val string) error {
-		e.Location = val
-		return nil
-	})
-	if err != nil {
-		slog.Default().Warn("Err was not nil when parsing optional event location", "error", err)
-		// This is purposefull empty because we should never get here since this isn't required
-	}
-	return nil
 }
 
 type Cal struct {
@@ -152,8 +76,8 @@ func (c Cal) Subscribe(url string, guildID string) error {
 
 	events := make([]Event, len(cal.Events()))
 	for i, event := range cal.Events() {
-		var e Event
-		err := e.ParseFromiCal(event)
+		var e types.Event
+		err := types.ParseFromiCal(event)
 		if err != nil {
 			c.logger.Error("error parsing ical event", slog.Any("event", event), slog.Any("error", err))
 			continue
